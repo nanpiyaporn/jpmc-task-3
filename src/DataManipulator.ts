3f2d47e4c76d594de7e15c6654314d1919b08c5b
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-02-27 00:42:01 -0500

    spill error fix

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -13,8 +13,8 @@ export interface Row {
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]):Row {
-    const priceABC=(serverRespond[0].top_ask.price + serverRespond[0].top_bit.price)/2;
-    const priceDEF=(serverRespond[1].top_ask.price + serverRespond[1].top_bit.price)/2;
+    const priceABC=(serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2;
+    const priceDEF=(serverResponds[1].top_ask.price + serverResponds[1].top_bid.price)/2;
     const ratio = priceABC/priceDEF;
     const upperBound = 1 + 0.05;
     const lowerBound = 1- 0.05;
@@ -23,10 +23,10 @@ export class DataManipulator {
       price_abc: priceABC,
       price_def: priceDEF,
       ratio,
-      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
-        serverRespond[0].timestamp : serverRespond[1].timestamp,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?
+        serverResponds[0].timestamp : serverResponds[1].timestamp,
       upper_bound: upperBound,
-      lower_bound: lowerBound,
+      lowwer_bound: lowerBound,
       trigger_alert:(ratio>upperBound || ratio<lowerBound) ? ratio : undefined,
     };
   
